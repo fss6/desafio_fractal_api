74b488054fb701736903a88d8a54cf404f3d4e57
Fábio Santos <fabiosantos.pe@gmail.com>
2017-12-23 21:24:21 -0300

    - Atividade 005: Teste unitarios modelo Project * Add testes unitatios

---
+++ spec/models/project_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe Project, type: :model do
+  before(:all) do
+    @user = User.first || create(:user)
+    @category = Category.first || create(:category, user_id: @user.id)
+    @project = Project.first ||
+      create(:project, user_id: @user.id, category_id: @category.id)
+  end
+
+  it "is valid with valid attributes" do
+    project = build(:project, name: generate(:name), user_id: @user.id, category_id: @category.id)
+    expect(project.valid?).to be_truthy
+  end
+
+  it "is nil name attribute" do
+    project = build(:project, name: nil, user_id: @user.id, category_id: @category.id)
+    expect(project.valid?).to be_falsey
+  end
+
+  it "is nil category attribute" do
+    project = build(:project, name: nil, user_id: @user.id)
+    expect(project.valid?).to be_falsey
+  end
+
+  it "is nil user attribute" do
+    project = build(:project, name: nil, category_id: @category.id)
+    expect(project.valid?).to be_falsey
+  end
+
+  it "has a unique name" do
+    project = build(:project, user_id: @user.id, category_id: @category.id)
+    expect(project.valid?).to be_falsey
+  end
+end
