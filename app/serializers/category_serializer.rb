67ee66a0c7c9d07ecc29100e33bebc377aceeb16
Fábio Santos <fabiosantos.pe@gmail.com>
2017-12-27 01:21:24 -0300

    heroku fixbug: remocao dos links das classes serializadas

--- app/serializers/category_serializer.rb
+++ app/serializers/category_serializer.rb
@@ -1,5 +1,4 @@
 class CategorySerializer < ActiveModel::Serializer
   attributes :id, :name, :created_at, :updated_at
-  link(:show) { category_url(object) }
   belongs_to :user
 end
--- app/serializers/project_serializer.rb
+++ app/serializers/project_serializer.rb
@@ -1,6 +1,5 @@
 class ProjectSerializer < ActiveModel::Serializer
   attributes :id, :name, :description, :created_at, :updated_at
-  link(:show) { project_url(object) }
 
   belongs_to :category do
     { id: object.category.id, name: object.category.name }
